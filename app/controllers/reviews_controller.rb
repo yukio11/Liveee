b1cefb3373302ece643e0a12d03c8bc5b08e1886
Yukihiro <yukihiro.ueda.1105@gmail.com>
2016-12-11 03:22:10 +0900

    reviewをliveページに表示

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,4 +6,14 @@ class ReviewsController < ApplicationController
     @live = Live.find(params[:life_id])
     @review = Review.new
   end
+
+  def create
+    Review.create(create_params)
+    redirect_to controller: :lives, action: :index
+  end
+
+  private
+    def create_params
+      params.require(:review).permit(:nickname, :rate, :review).merge(live_id: params[:life_id])
+    end
 end
