16e3030d3dd2777b78fb7f49ce69416d7434e6e5
Yukihiro <yukihiro.ueda.1105@gmail.com>
2016-12-11 08:10:58 +0900

    システム大枠完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'lives#index'
+  resources :users, only: :show
   resources :lives do
     resources :reviews, only: [:new, :create]
     collection do
