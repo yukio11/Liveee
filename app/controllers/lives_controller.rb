59c6e3e603ff412f128484687a57bb2485c2e2b6
Yukihiro <yukihiro.ueda.1105@gmail.com>
2016-12-19 15:24:00 +0900

    キーワード検索

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -2,7 +2,7 @@ class LivesController < ApplicationController
   before_action :ranking
 
   def index
-    @q = Live.search(params[:q])
+    @q = Live.ransack(params[:q])
     @lives = @q.result(distinct: true)
     # @lives = Live.all.page(params[:page]).per(10)
   end
@@ -15,7 +15,7 @@ class LivesController < ApplicationController
 
   def search
     # @search_lives = Live.where('title LIKE(?)', "%#{params[:keyword]}%").page(params[:page]).per(10)
-    @q = Live.search(params[:q])
+    @q = Live.ransack(params[:q])
     @search_lives = @q.result(distinct: true)
     # @search_lives = Live.search(:title_cont => '%#{params[:keyword]}%').result.page(params[:page]).per(10)
   end
