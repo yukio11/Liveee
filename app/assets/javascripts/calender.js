4512124af6514b0f8d2efd207f73d03dcbc87007
Yukihiro <yukihiro.ueda.1105@gmail.com>
2016-12-21 17:43:13 +0900

    とりあえずコミット

---
+++ app/assets/javascripts/calender.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#calendar').fullCalendar({
+    })
+
+  // $('#calendar').fullCalendar('addEventSource', [{
+  // id:date,      (date それぞれのボックスが持つ日付。）
+  // title: title,
+  // start: date,　（日付）
+  // allDay: allDay
+  // }]);
+
+});
